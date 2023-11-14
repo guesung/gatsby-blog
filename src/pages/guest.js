d4964170bc2db5323b5176f1bcd531aa6a56d046
Kuesung Park <gueit214@naver.com>
2023-11-14 22:03:02 +0900

    feat : ApolloProvider로 client 처리

--- src/pages/guest.js
+++ src/pages/guest.js
@@ -4,17 +4,26 @@ import { graphql } from 'gatsby'
 import ChatList from '../components/chat-list'
 import ChatForm from '../components/chat-form'
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000', // Replace with your Apollo Server URL
+  cache: new InMemoryCache(),
+})
+
 export default function guest({ location, data }) {
   const { siteMetadata } = data.site
 
   return (
-    <Layout location={location} title={siteMetadata.title}>
-      <h1>Guest Book</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <ChatList />
-      </Suspense>
-      <ChatForm />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout location={location} title={siteMetadata.title}>
+        <h1>Guest Book</h1>
+        <Suspense fallback={<p>Loading...</p>}>
+          <ChatList />
+        </Suspense>
+        <ChatForm />
+      </Layout>
+    </ApolloProvider>
   )
 }
 
