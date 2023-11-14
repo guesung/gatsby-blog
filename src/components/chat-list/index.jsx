240527b96a2915323d4f3a26f3d469b095b94cc8
Kuesung Park <gueit214@naver.com>
2023-11-14 22:10:34 +0900

    fix : import

--- src/components/chat-list/index.jsx
+++ src/components/chat-list/index.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import './index.scss'
 
 import { gql, useSuspenseQuery } from '@apollo/client'
-import { getApolloClient } from '../../utils/getApolloClient'
 
 const GET_DATA = gql`
   query GetData {
