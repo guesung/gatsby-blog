f3fa789358f2fc12d5b61e3bac0b9ab2a6a33c29
Kuesung Park <gueit214@naver.com>
2023-11-16 10:24:26 +0900

    feat : 메시지 입력 후 가장 채팅창 하단으로 이동

--- src/components/chat-list/index.jsx
+++ src/components/chat-list/index.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import './index.scss'
 
 export default function ChatList({ chatList }) {
+  const messageEndRef = useRef(null)
+  useEffect(() => {
+    messageEndRef.current.scrollIntoView({ behavior: 'smooth' })
+  }, [chatList])
+
   return (
     <section className="chat-room">
       {chatList.chats.map((chat, index) => (
         <Chat chat={chat} key={index} />
       ))}
+      <div ref={messageEndRef} />
     </section>
   )
 }
